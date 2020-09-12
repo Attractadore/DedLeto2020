a93db5e74ea62243cd7ac35309e97b79922a3d2e
Michael <attractadore02@gmail.com@>
2020-09-12 20:05:26 +0300

    Update assign_lines

--- stringsort/lines.c
+++ stringsort/lines.c
@@ -20,27 +20,28 @@ size_t sanitize_buffer(char** buffer_ptr, size_t buffer_size) {
     return buffer_size;
 }
 
-size_t assign_lines(char*** lines_ptr, size_t lines_buffer_size, char* string_buffer, size_t string_buffer_size){
+size_t assign_lines(char*** lines_ptr, char* string_buffer, size_t string_buffer_size){
     assert(lines_ptr);
     assert(string_buffer);
 
-    *lines_ptr = NULL;
-
+    size_t lines_buffer_size = strncnt(string_buffer, string_buffer_size, "\n", 1);
     char** lines = calloc(lines_buffer_size, sizeof(*lines));
+    *lines_ptr = lines;
     if (!lines) {
         return 0;
     }
 
-    *lines_ptr = lines;
-
     size_t num_read_lines = 0;
-    char p = '\0';
+    char p = '\n';
     for (size_t i = 0; i < string_buffer_size; i++) {
-        if (p == '\0') {
+        if (p == '\n') {
             lines[num_read_lines++] = string_buffer + i;
         }
         p = string_buffer[i];
     }
+
+    assert(num_read_lines == lines_buffer_size);
+
     return num_read_lines;
 }
 
@@ -61,24 +62,21 @@ LINES* read_lines(FILE* input_file) {
 
     assert(string_buffer);
 
-    size_t lines_buffer_size = strncnt(string_buffer, string_buffer_size, "\n", 1);
-    {
-        size_t replace_sbuffer_size = strnrep(string_buffer, string_buffer_size, "\n", 1, "\0", 1);
-        if (!replace_sbuffer_size) {
-            free(string_buffer);
-            return NULL;
-        }
-        assert(replace_sbuffer_size == string_buffer_size);
-    }
-
     char** lines = NULL;
-    size_t num_read_lines = assign_lines(&lines, lines_buffer_size, string_buffer, string_buffer_size);
+    size_t num_read_lines = assign_lines(&lines, string_buffer, string_buffer_size);
     if (!num_read_lines) {
         free(lines);
         free(string_buffer);
         return NULL;
     }
-    assert(num_read_lines == lines_buffer_size);
+
+    size_t replace_sbuffer_size = strnrep(string_buffer, string_buffer_size, "\n", 1, "\0", 1);
+    if (!replace_sbuffer_size) {
+        free(lines);
+        free(string_buffer);
+        return NULL;
+    }
+    assert(replace_sbuffer_size == string_buffer_size);
 
     LINES* lines_s = calloc(1, sizeof(*lines_s));
     if (!lines_s) {
